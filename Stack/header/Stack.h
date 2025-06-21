58ccef94f41f098e9bc298bc7dd56827dc346fd8
DESKTOP-ROOGGBN\Smersh <dbtjdtn325@gmail.com>
2025-06-22 02:48:43 +0900

    refractoring stack

--- Stack/header/Stack.h
+++ Stack/header/Stack.h
@@ -1,22 +1,34 @@
 #pragma once
 
+#include "Libs.h"
+#include "ListNode.h"
 #include "List.h"
+// stack
 
 class Stack : public List
 {
-    public:
-    Stack();
-    virtual ~Stack();
+public:
+	/*Management*/
+	Stack();
+	~Stack();
 
-    void Stack_init();
-    int Stack_push(MyAddr* Data);
-    int Stack_pop(MyAddr** Data);
-    int Stack_peek(MyAddr* bucket);
+	bool Push(void* data);
+	bool Pop(void** data);
+	const void* Peek();
 
-    int Stack_getStackSize();
-    virtual int Stack_cmpData(const char* key1, const char* key2);
-    ErrCode Stack_getErrCode();
-    
-    private:
+	/*Utility*/
+protected:
+	/*Utility-1*/
 
+	/*Accessor*/
+private:
+
+	/* Hide */
+	using List::set_Data;
+	using List::get_Data;
+	using List::lookup_Node;
+	using List::push_back;
+	using List::insert_nextNode;
+	using List::remove_nextNode;
 };
+
